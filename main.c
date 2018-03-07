bbc390190e8bdde0c19310b7b03688a234f91a5b
Gratzounias <georger.chris.litsas@gmail.com>
2018-03-07 13:10:01 +0200

    main edit

--- main.c
+++ main.c
@@ -15,23 +15,15 @@ int main(int argc, char * argv[])
 	char * methodosAntikatastashs = malloc(4*sizeof(char));
 	int plh8osSelidwnMnhmhs,para8yro,q,megistoPlh8osDieu8ynsewn;
 
-	fp1 = fopen("bzip.trace","r");
-	fp2 = fopen("gcc.trace","r");
+	fp1 = fopen("text1","r");
+	fp2 = fopen("text2","r");
 
 	strcpy(methodosAntikatastashs,argv[1]);
 	plh8osSelidwnMnhmhs = atoi(argv[2]);
 	q = atoi(argv[3]);
 	megistoPlh8osDieu8ynsewn = atoi(argv[4]);
 
-	if( strcmp(methodosAntikatastashs,"WS") == 0 )
-	{
-		if( argc == 6 )
-			para8yro = atoi(argv[5]);
-		else
-			printf("Lathos kata thn eisodo parametrwn");
-	}
-	else
-		para8yro = -1;
+	
 
 	printf("Dwsate ta ekseis orimsata\n");
 	printf("Me8odos antikatastashs : %s\n",methodosAntikatastashs);
@@ -45,8 +37,8 @@ int main(int argc, char * argv[])
 
 	arxikopoihshPageTable(pageTable,plh8osSelidwnMnhmhs,3);
 
-	char dieu8ynsh[8];
-	char leitourgia;
+	char dieu8ynsh[8];// antikatastash me int
+	char leitourgia; // den xreiazetai
 
 	int plh8osDieu8ynsewnPouExwDiavasei = 0;
 
@@ -54,25 +46,33 @@ int main(int argc, char * argv[])
 
 	do{
 
-		int metrhthsGiaToQ = 0;
-
-		while( metrhthsGiaToQ < q )
+		int metrhthsGiaToQ = 0; //i
+		//int j=0;
+		while( metrhthsGiaToQ < q ) // i< EOF
 		{
 			
 			fscanf(fp1,"%s",dieu8ynsh); // diavase th die8ynsh
 			fgetc(fp1); // diavazei to keno gia na mporesw meta na diavasw to R h' to W
 			fscanf(fp1,"%c",&leitourgia); // diavase th leitourgia auths ths die8ynshs
 
-			vresThSelida(ari8mosSelidas,dieu8ynsh); // vres ton ari8mo ths selidas
-			pros8eseSelidaStoPageTable(pageTable,plh8osSelidwnMnhmhs,ari8mosSelidas,leitourgia,methodosAntikatastashs,para8yro); // pros8ese th selida sth mnhmh
+			/*vresThSelida(ari8mosSelidas,dieu8ynsh); // vres ton ari8mo ths selidas
+			pros8eseSelidaStoPageTable			 
+	(pageTable,plh8osSelidwnMnhmhs,ari8mosSelidas,leitourgia,methodosAntikatastashs,para8yro); // pros8ese th selida sth mnhmh
 
 			plh8osDieu8ynsewnPouExwDiavasei++;
-			metrhthsGiaToQ++;
+			metrhthsGiaToQ++;*/
+
+			//j++;
+			//if(j!=dieu8ynsh){
+			//		printf("La8os kata thn eisagwgh stoixeiwn. Termatismos...\n");
+			//		exit(3);			
+			//		}
+
 		}
 
-		metrhthsGiaToQ = 0;
+		metrhthsGiaToQ = 0; //i
 
-		while( metrhthsGiaToQ < q )
+		while( metrhthsGiaToQ < q ) // i<EOF
 		{
 			fscanf(fp2,"%s",dieu8ynsh); // diavase th die8ynsh
 			fgetc(fp2); // diavazei to keno gia na mporesw meta na diavasw to R h' to W
